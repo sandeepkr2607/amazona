2e5ba925dd7392ea9f214491c471f40f3a9d4f01
sandeep-nixon <sandeepkr2607@gmail.com>
2023-01-13 18:01:41 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> Amazona</a>
+      </header>
+      <main>ListProducts</main>
+    </div>
+  );
+}
+
+export default App;
